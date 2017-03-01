361e09246fce82f53532780c702e0c407cc4b1ff
KenChiu <knowledge0210@gmail.com>
2017-03-01 16:17:45 +0800

    fist commit

---
+++ MessageCenterDemo/DemoModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MessageCenterDemo
+{
+	public class DemoModel : BaseViewModel
+	{
+		public string Text { get; set; }
+
+		private Command buttonCommand;
+		public Command ButtonCommand
+		{
+			get { return buttonCommand ?? (buttonCommand = new Command(() => showDisplayAlert())); }
+		}
+
+		private void showDisplayAlert()
+		{
+			MessagingCenter.Send(this, "Alert");
+		}
+
+		private Command buttonMessageCommand;
+		public Command ButtonMessageCommand
+		{
+			get { return buttonMessageCommand ?? (buttonMessageCommand = new Command(() => showMessageDisplayAlert())); }
+		}
+
+		private void showMessageDisplayAlert()
+		{
+			MessagingCenter.Send(this, "AlertMessage", Text);
+		}
+	}
+}
+
---
+++ MessageCenterDemo/DemoPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MessageCenterDemo
+{
+	public partial class DemoPage : ContentPage
+	{
+		public DemoPage()
+		{
+			InitializeComponent();
+			BindingContext = new DemoModel();
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+			SubscribeMessage();
+		}
+
+		protected override void OnDisappearing()
+		{
+			base.OnDisappearing();
+			UnSubscribeMessage();
+		}
+
+		private void SubscribeMessage()
+		{
+			MessagingCenter.Subscribe<DemoModel>(this, "Alert", async (obj) =>
+			{
+				  await DisplayAlert("Message", "Hello World", "OK");
+			});
+
+			MessagingCenter.Subscribe<DemoModel, string>(this, "AlertMessage", async (arg1, arg2) =>
+			{
+				   await DisplayAlert("Message", arg2, "OK");
+			});
+		}
+
+		private void UnSubscribeMessage()
+		{
+			MessagingCenter.Unsubscribe<DemoModel>(this, "Alert");
+			MessagingCenter.Unsubscribe<DemoPage>(this, "AlertMessage");
+		}
+	}
+}
